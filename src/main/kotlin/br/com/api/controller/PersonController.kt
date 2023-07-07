d64e508cff92df8f89d3924fade12612100c5b44
carlos731 <chtsca2001@gmail.com>
2023-07-07 00:29:00 -0300

    Seção 7 - Adicionando projeto com implementação de metodos mockados

---
+++ src/main/kotlin/br/com/api/controller/PersonController.kt
@@ -0,0 +1,62 @@
+package br.com.api.controller
+
+import br.com.api.model.Person
+import br.com.api.services.PersonService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.*
+import java.util.concurrent.atomic.AtomicLong
+
+@RestController
+@RequestMapping("/person")
+class PersonController {
+
+    val counter: AtomicLong = AtomicLong()
+
+    @Autowired
+    private lateinit var service: PersonService
+    //var service: PersonService = PersonService() // Igual ao código de cima so que não precisa mais.
+
+    @RequestMapping(
+        method = [RequestMethod.GET],
+        produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun findAll(): List<Person> {
+        return service.findAll()
+    }
+
+    @RequestMapping(
+        value = ["/{id}"],
+        method = [RequestMethod.GET],
+        produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun findById(@PathVariable(value = "id") id: Long): Person {
+        return service.findById(id)
+    }
+
+    @RequestMapping(
+        method = [RequestMethod.POST],
+        consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun create(@RequestBody person: Person): Person {
+        return service.create(person)
+    }
+
+    @RequestMapping(
+        method = [RequestMethod.PUT],
+        consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun update(@RequestBody person: Person): Person {
+        return service.update(person)
+    }
+
+    @RequestMapping(
+        value = ["/{id}"],
+        method = [RequestMethod.DELETE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun delete(@PathVariable(value = "id") id: Long) {
+        return service.delete(id)
+    }
+
+}
---
+++ src/main/kotlin/br/com/api/services/PersonService.kt
@@ -0,0 +1,56 @@
+package br.com.api.services
+
+import br.com.api.model.Person
+import org.springframework.stereotype.Service
+import java.util.concurrent.atomic.AtomicLong
+import java.util.logging.Logger
+
+@Service
+class PersonService {
+    private val counter: AtomicLong = AtomicLong()
+
+    private val logger = Logger.getLogger(PersonService::class.java.name)
+
+    fun findAll(): List<Person> {
+        logger.info("Finding all people!")
+
+        val persons: MutableList<Person> = ArrayList()
+        for (i in 0 .. 7) {
+            val person = mockPerson(i)
+            persons.add(person)
+        }
+        return persons
+    }
+
+    fun findById(id: Long): Person {
+        logger.info("Finding one person!")
+
+        val person = Person()
+        person.id = counter.incrementAndGet()
+        person.firstName = "Carlos"
+        person.lastName = "Henrique"
+        person.address = "Rio de Janeiro - Brasil"
+        person.gender = "Male"
+        return person
+    }
+
+    fun create(person: Person) = person
+
+    fun update(person: Person) = person
+
+    fun delete(id: Long) {
+
+    }
+
+    private fun mockPerson(i: Int): Person {
+        val person = Person()
+        person.id = counter.incrementAndGet()
+        person.firstName = "Person Name $i"
+        person.lastName = "lastName $i"
+        person.address = "Some Address in Brasil"
+        person.gender = "Male"
+        return person
+    }
+
+
+}
